3a6a12baf36f8b1660442f7fbe08991608a336d9
kh990105 <kh990105@naver.com>
2022-10-12 22:27:11 +0900

    intial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(function(){
+    $('.search-btn').click(function(e){
+        e.preventDefault();
+        $('.searchForm,.input-search').toggleClass('act');
+    });
+$('.gnb>li').hover(function(){
+    $(this).find('.lnb').slideToggle();
+});
+$('.mobile-nav-box>li').click(function(){
+    if($(this).find('fa-solid').hasClass('fa-angle-down')){
+        $(this).find('fa-solid').removeClass('fa-angle-down').addClass('fa-angle-up');
+    }else{
+        $(this).find('fa-solid').removeClass('fa-angle-up').addClass('fa-angle-down');
+    }
+    $(this).find('.mobile-lnb').slideToggle();
+  })
+
+$('.menu').click(function(){
+    $('.wrapper').css({
+        position:'absolute',
+        top:0,
+        left:0,
+        width:'100%'
+    }).animate({
+        left:'-70%'
+    }, 500);
+});
+  /*내꺼*/
+$('.menu, .xclose').click(function(){  
+    $('.fadeblack').animate({width:"toggle"},500);
+  })
+  $('.xclose').click(function(){
+    $('.wrapper').animate({
+        left:0
+    },500,function(){
+        $('.wrapper').css('position','relative');
+    })
+    })
+});
